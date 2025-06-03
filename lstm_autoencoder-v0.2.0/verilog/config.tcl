09d8e94144555122712e23754c8d684ea837f541
Niklas Anderson <nkanders@gmail.com>
2025-06-03 14:39:56 -0700

    Update config to use json file The tcl file has the keys that were not able to be transferred to json uncommented. This file will be removed, but checking in this change for documentation purposes. Also adds the SRAM macro verilog file.

--- lstm_autoencoder-v0.2.0/verilog/config.tcl
+++ lstm_autoencoder-v0.2.0/verilog/config.tcl
@@ -1,35 +1,29 @@
-set ::env(DESIGN_NAME) matvec_mult_top
 
-set ::env(CLOCK_PORT) "clk"
-set ::env(CLOCK_PERIOD) "40.0"
-
-# Include all source files
-set ::env(VERILOG_FILES) [glob $::env(DESIGN_DIR)/src/*.v]
 
 # Floorplan defaults
 # FP_CORE_UTIL may be increased to reduce cell density and
 # make routing easier, but this will increase area.
-set ::env(FP_CORE_UTIL) 60
+# set ::env(FP_CORE_UTIL) 60
 set ::env(FP_ASPECT_RATIO) 1.0
 set ::env(FP_CORE_MARGIN) 2
-set ::env(PL_TARGET_DENSITY) 0.55
+# set ::env(PL_TARGET_DENSITY) 0.55
 
 set ::env(SYNTH_MAX_FANOUT) 20
 
 # Specify macro placement
 # May try to let OpenLane try to handle this by commenting out the
 # macro_placement.cfg file
-set ::env(MACRO_PLACEMENT_CFG) $::env(DESIGN_DIR)/macro_placement.cfg
+# set ::env(MACRO_PLACEMENT_CFG) $::env(DESIGN_DIR)/macro_placement.cfg
 set ::env(PL_MACROS_GRID) 1
 
 # SRAM macro files
-set ::env(EXTRA_LEFS) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8.lef"
-set ::env(EXTRA_GDS_FILES) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8.gds"
-lappend ::env(LIB_FILES) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8_TT_1p8V_25C.lib"
+# set ::env(EXTRA_LEFS) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8.lef"
+# set ::env(EXTRA_GDS_FILES) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8.gds"
+# lappend ::env(LIB_FILES) "$::env(DESIGN_DIR)/macro/sky130_sram_2kbyte_1rw1r_32x512_8/sky130_sram_2kbyte_1rw1r_32x512_8_TT_1p8V_25C.lib"
 
 # Power/Ground nets for this design
-set ::env(VDD_PIN) "vccd1"
-set ::env(GND_PIN) "vssd1"
+# set ::env(VDD_PIN) "vccd1"
+# set ::env(GND_PIN) "vssd1"
 
 # Reducing power straps to attempt to reduce congestion and routing blockages
 set ::env(FP_PDN_VPITCH) 40
