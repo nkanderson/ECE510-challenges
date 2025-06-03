bf020f4f8a46853c13ea28e724c87b99fe26f481
Niklas Anderson <nkanders@gmail.com>
2025-06-03 16:41:41 -0700

    Fixup conflicting SRAM macro definitions

--- lstm_autoencoder-v0.2.0/verilog/src/sky130_sram_2kbyte_1rw1r_32x512_8.v
+++ lstm_autoencoder-v0.2.0/verilog/src/sky130_sram_2kbyte_1rw1r_32x512_8.v
@@ -58,11 +58,11 @@ module sky130_sram_2kbyte_1rw1r_32x512_8(
     din0_reg = din0;
 `ifndef SYNTHESIS
     #(T_HOLD) dout0 = 32'bx;
-`endif
     if ( !csb0_reg && web0_reg && VERBOSE ) 
       $display($time," Reading %m addr0=%b dout0=%b",addr0_reg,mem[addr0_reg]);
     if ( !csb0_reg && !web0_reg && VERBOSE )
       $display($time," Writing %m addr0=%b din0=%b wmask0=%b",addr0_reg,din0_reg,wmask0_reg);
+`endif
   end
 
   reg  csb1_reg;
@@ -74,13 +74,13 @@ module sky130_sram_2kbyte_1rw1r_32x512_8(
   begin
     csb1_reg = csb1;
     addr1_reg = addr1;
+`ifndef SYNTHESIS
     if (!csb0 && !web0 && !csb1 && (addr0 == addr1))
          $display($time," WARNING: Writing and reading addr0=%b and addr1=%b simultaneously!",addr0,addr1);
-`ifndef SYNTHESIS
     #(T_HOLD) dout1 = 32'bx;
-`endif
     if ( !csb1_reg && VERBOSE ) 
       $display($time," Reading %m addr1=%b dout1=%b",addr1_reg,mem[addr1_reg]);
+`endif
   end
 
 reg [DATA_WIDTH-1:0]    mem [0:RAM_DEPTH-1];
